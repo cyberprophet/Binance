243673115a6b5b1404164e468149deb1b1f8a65a
SANGWOO PARK <cyberprophet@hanmail.net>
2024-05-25 13:26:38 +0900

    publish NuGet Gallery

---
+++ Models/RealTicker.Mini.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace ShareInvest.Binance.Models;
+
+public class RealMiniTicker
+{
+    [DataMember, JsonProperty("e"), JsonPropertyName("eventType")]
+    public string? EventType
+    {
+        get; set;
+    }
+
+    [DataMember, JsonProperty("E"), JsonPropertyName("eventTime")]
+    public long EventTime
+    {
+        get; set;
+    }
+
+    /// <summary>Symbol</summary>
+    [DataMember, JsonProperty("s"), JsonPropertyName("symbol")]
+    public string? Code
+    {
+        get; set;
+    }
+
+    [DataMember, JsonProperty("c"), JsonPropertyName("closePrice")]
+    public string? ClosePrice
+    {
+        get; set;
+    }
+
+    [DataMember, JsonProperty("o"), JsonPropertyName("openPrice")]
+    public string? OpenPrice
+    {
+        get; set;
+    }
+
+    [DataMember, JsonProperty("h"), JsonPropertyName("highPrice")]
+    public string? HighPrice
+    {
+        get; set;
+    }
+
+    [DataMember, JsonProperty("l"), JsonPropertyName("lowPrice")]
+    public string? LowPrice
+    {
+        get; set;
+    }
+
+    /// <summary>Total traded base asset volume</summary>
+    [DataMember, JsonProperty("v"), JsonPropertyName("baseAssetVolume")]
+    public string? BaseAssetVolume
+    {
+        get; set;
+    }
+
+    /// <summary>Total traded quote asset volume</summary>
+    [DataMember, JsonProperty("q"), JsonPropertyName("quoteAssetVolume")]
+    public string? QuoteAssetVolume
+    {
+        get; set;
+    }
+}
---
+++ Models/RealTicker.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace ShareInvest.Binance.Models;
+
+public class RealTicker : RealMiniTicker
+{
+    /// <summary>Price change</summary>
+    [DataMember, JsonProperty("p"), JsonPropertyName("priceChange")]
+    public string? PriceChange
+    {
+        get; set;
+    }
+
+    /// <summary>Price change percent</summary>
+    [DataMember, JsonProperty("P"), JsonPropertyName("priceChangePercent")]
+    public string? PriceChangePercent
+    {
+        get; set;
+    }
+
+    /// <summary>Weighted average price</summary>
+    [DataMember, JsonProperty("w"), JsonPropertyName("weightedAvgPrice")]
+    public string? WeightedAvgPrice
+    {
+        get; set;
+    }
+
+    /// <summary>First trade(F)-1 price (first trade before the 24hr rolling window)</summary>
+    [DataMember, JsonProperty("x"), JsonPropertyName("firstTradePrice")]
+    public string? FirstTradePrice
+    {
+        get; set;
+    }
+
+    /// <summary>Last quantity</summary>
+    [DataMember, JsonProperty("Q"), JsonPropertyName("lastQty")]
+    public string? LastQty
+    {
+        get; set;
+    }
+
+    /// <summary>Best bid price</summary>
+    [DataMember, JsonProperty("b"), JsonPropertyName("bestBidPrice")]
+    public string? BestBidPrice
+    {
+        get; set;
+    }
+
+    /// <summary>Best bid quantity</summary>
+    [DataMember, JsonProperty("B"), JsonPropertyName("bestBidQty")]
+    public string? BestBidQty
+    {
+        get; set;
+    }
+
+    /// <summary>Best ask price</summary>
+    [DataMember, JsonProperty("a"), JsonPropertyName("bestAskPrice")]
+    public string? BestAskPrice
+    {
+        get; set;
+    }
+
+    /// <summary>Best ask quantity</summary>
+    [DataMember, JsonProperty("A"), JsonPropertyName("bestAskQty")]
+    public string? BestAskQty
+    {
+        get; set;
+    }
+
+    /// <summary>Statistics open time</summary>
+    [DataMember, JsonProperty("O"), JsonPropertyName("openTime")]
+    public long StatisticsOpenTime
+    {
+        get; set;
+    }
+
+    /// <summary>Statistics close time</summary>
+    [DataMember, JsonProperty("C"), JsonPropertyName("closeTime")]
+    public long StatisticsCloseTime
+    {
+        get; set;
+    }
+
+    /// <summary>First trade ID</summary>
+    [DataMember, JsonProperty("F"), JsonPropertyName("firstTradeId")]
+    public long FirstTradeId
+    {
+        get; set;
+    }
+
+    /// <summary>Total number of trades</summary>
+    [DataMember, JsonProperty("n"), JsonPropertyName("totalNumberOfTrades")]
+    public long TotalNumberOfTrades
+    {
+        get; set;
+    }
+
+    /// <summary>Last trade ID</summary>
+    [DataMember, JsonProperty("L"), JsonPropertyName("lastTradeId")]
+    public long LastTradeId
+    {
+        get; set;
+    }
+}
